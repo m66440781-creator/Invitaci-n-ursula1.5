c044b077bdc905536ace77f49e6104dc7c8b5c71
m66440781-creator <m66440781@gmail.com>
2026-08-08 03:52:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -591,3 +591,58 @@ function crearEstrellas(cantidad){
     }
 
 }
+
+// ===================================
+// CUENTA REGRESIVA
+// ===================================
+
+const fechaEvento = new Date("2026-08-16T18:30:00-06:00").getTime();
+
+function actualizarCuenta(){
+
+    const ahora = new Date().getTime();
+
+    const diferencia = fechaEvento - ahora;
+
+    if(diferencia <= 0){
+
+        document.getElementById("cuentaRegresiva").innerHTML =
+        "🍓 ¡Hoy es el gran día! 🍓";
+
+        return;
+
+    }
+
+    const dias = Math.floor(
+        diferencia / (1000 * 60 * 60 * 24)
+    );
+
+    const horas = Math.floor(
+        (diferencia % (1000 * 60 * 60 * 24)) /
+        (1000 * 60 * 60)
+    );
+
+    const minutos = Math.floor(
+        (diferencia % (1000 * 60 * 60)) /
+        (1000 * 60)
+    );
+
+    const segundos = Math.floor(
+        (diferencia % (1000 * 60)) /
+        1000
+    );
+
+
+    document.getElementById("dias").textContent = dias;
+
+    document.getElementById("horas").textContent = horas;
+
+    document.getElementById("minutos").textContent = minutos;
+
+    document.getElementById("segundos").textContent = segundos;
+
+}
+
+actualizarCuenta();
+
+setInterval(actualizarCuenta,1000);
